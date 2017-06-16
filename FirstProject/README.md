8f88b02e59d07d12c462395f545c51dca3aef2f5
Alexander Russell <awjrussell@live.co.uk>
2017-06-16 13:33:11 +0100

    Starter files for the first project

---
+++ FirstProject/README.md
@@ -0,0 +1,5 @@
+# First Project
+## Incepted by Muhammad Waqas Raza
+## Built by Soffia Bowring, Wouter Van Rijn & Hannah Phillips
+
+![Screenshot](screenshots/EnergyCalculatorScreenShot.jpg "First Screen Cap")
---
+++ FirstProject/logic.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+function sum() {
+    var myElec = document.getElementById("myElec").value;
+    var myGas = document.getElementById("myGas").value;
+    var Total = parseInt(myGas) + parseInt(myElec);
+    document.getElementById("Total").value = Total;
+}
